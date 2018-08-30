033e0871143f35b920f92a4dbd5bf939aa1a0509
Rob Nash <robscode@icloud.com>
2018-08-30 21:09:13 +0100

    Slide animation now working with rotation changes.

--- Sheet/SlideViewController.swift
+++ Sheet/SlideViewController.swift
@@ -47,8 +47,11 @@ final class SlideViewController: ParentViewController {
         }
         centerXAnchor1 = constraints.first
         NSLayoutConstraint.activate(constraints)
+        currentConstraints = constraints
     }
-
+    
+    private var currentConstraints: [NSLayoutConstraint]?
+    
     override func show(_ vc: UIViewController, sender: Any?) {
         
         let child = childViewControllers.last!
@@ -58,31 +61,47 @@ final class SlideViewController: ParentViewController {
         addChildViewController(vc)
         vc.view!.translatesAutoresizingMaskIntoConstraints = false
         
+        if let child = childViewControllers.last {
+            for constraint in child.view!.constraints {
+                if let firstItem = constraint.firstItem as? UIView {
+                    let firstAnchor = constraint.firstAnchor
+                    let a = firstItem == child.view!
+                    let b = firstAnchor.isEqual(centerXAnchor2)
+                    if a && b {
+                        centerXAnchor1 = constraint
+                        break
+                    }
+                }
+            }
+        }
+        
         if traitCollection.verticalSizeClass == .regular || traitCollection.verticalSizeClass == .unspecified {
             let constraints = portraitConstraints(vc)
             NSLayoutConstraint.activate(constraints)
             centerXAnchor2 = constraints.first
+            currentConstraints = constraints
         } else {
             let constraints = landscapeConstraints(vc)
             NSLayoutConstraint.activate(constraints)
             centerXAnchor2 = constraints.first
+            currentConstraints = constraints
         }
         
         let width = view.bounds.width
         
         switch animation {
         case .slideRight:
-            centerXAnchor2?.constant -= width
+            centerXAnchor2!.constant -= width
             vc.view!.layoutIfNeeded()
             view.layoutIfNeeded()
-            centerXAnchor1?.constant += width
-            centerXAnchor2?.constant += width
+            centerXAnchor1!.constant += width
+            centerXAnchor2!.constant += width
         case .slideLeft:
-            centerXAnchor2?.constant += width
+            centerXAnchor2!.constant += width
             vc.view!.layoutIfNeeded()
             view.layoutIfNeeded()
-            centerXAnchor1?.constant -= width
-            centerXAnchor2?.constant -= width
+            centerXAnchor1!.constant -= width
+            centerXAnchor2!.constant -= width
         }
         
         UIView.animate(withDuration: 0.3, animations: {
@@ -93,5 +112,25 @@ final class SlideViewController: ParentViewController {
             vc.didMove(toParentViewController: self)
             self.didShow?()
         }
+    }
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.willTransition(to: newCollection, with: coordinator)
+        guard let child = childViewControllers.last else { return }
+        NSLayoutConstraint.deactivate(currentConstraints!)
+        if newCollection.verticalSizeClass == .regular || newCollection.verticalSizeClass == .unspecified {
+            let constraints = portraitConstraints(child)
+            NSLayoutConstraint.activate(constraints)
+            centerXAnchor1 = constraints.first
+            currentConstraints = constraints
+        } else {
+            let constraints = landscapeConstraints(child)
+            NSLayoutConstraint.activate(constraints)
+            centerXAnchor1 = constraints.first
+            currentConstraints = constraints
+        }
+        coordinator.animate(alongsideTransition: { [unowned self] _ in
+            self.view.layoutIfNeeded()
+            }, completion: nil)
     }
 }
