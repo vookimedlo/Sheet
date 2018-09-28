094ee9d80c73d2f7c8d93cb7daa96e04696ad023
Rob Nash <robscode@icloud.com>
2018-09-28 09:35:22 +0100

    Update demo

--- Sender/ViewController.swift
+++ Sender/ViewController.swift
@@ -3,15 +3,21 @@ import Sheet
 
 class ViewController: UIViewController {
 
-    private let sheetManager = SheetManager(animation: .custom)
+    private let sheetManager = Sheet(animation: .custom, widthInset: 30, bottomInset: 10)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         sheetManager.chromeTapped = { [unowned self] in
             self.dismiss(animated: true)
         }
-        NotificationCenter.default.addObserver(forName: .dismiss, object: nil, queue: nil) { [weak self] _ in
-            self?.dismiss(animated: true)
+        NotificationCenter.default.addObserver(forName: .dismiss, object: nil, queue: nil) { [weak self] notification in
+            let animated: Bool
+            if let info = notification.userInfo, let outcome = info[dismissSheetAnimatedKey] as? Bool {
+                animated = outcome
+            } else {
+                animated = true
+            }
+            self?.dismiss(animated: animated)
         }
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapGestureRecognized(_:)))
         view.addGestureRecognizer(tapGesture)
@@ -19,7 +25,8 @@ class ViewController: UIViewController {
     
     @objc func tapGestureRecognized(_ sender: UITapGestureRecognizer) {
         
-        // when using SheetManager(animation: .custom) && custom segue
+        // when using Sheet(animation: .custom) && custom segue
+        // also look at notes in WelcomeSheetViewController
         let viewController = UIStoryboard(name: "Custom", bundle: nil).instantiateInitialViewController()!
         sheetManager.show(viewController, above: self)
         
